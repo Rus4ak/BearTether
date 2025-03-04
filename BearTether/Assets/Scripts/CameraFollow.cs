60f1b310bc6f331a12379a3a9a0016086984656a
rus4ak <rus4ak331@gmail.com>
2025-03-04 21:42:19 +0200

    Add camera borders

--- BearTether/Assets/Scripts/CameraFollow.cs
+++ BearTether/Assets/Scripts/CameraFollow.cs
@@ -4,11 +4,24 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform _target;
     [SerializeField] private float _smoothSpeed = 8;
+    [SerializeField] private Transform _minPos;
+    [SerializeField] private Transform _maxPos;
+
+    private float _cameraWidth, _cameraHeight;
+
+    private void Start()
+    {
+        _cameraHeight = Camera.main.orthographicSize;
+        _cameraWidth = _cameraHeight * Screen.width / Screen.height;
+    }
 
     private void FixedUpdate()
     {
         Vector3 pos = Vector3.Lerp(transform.position, _target.position, _smoothSpeed * Time.fixedDeltaTime);
         pos.z = -10;
+
+        pos.x = Mathf.Clamp(pos.x, _minPos.position.x + _cameraWidth, _maxPos.position.x - _cameraWidth);
+        pos.y = Mathf.Clamp(pos.y, _minPos.position.y + _cameraHeight, _maxPos.position.y - _cameraHeight);
 
         transform.position = pos;
     }
