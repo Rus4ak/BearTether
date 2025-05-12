e2a3b08ced562fb3a3bc46b6894628a5cd08c936
rus4ak <rus4ak331@gmail.com>
2025-05-12 20:58:12 +0300

    Add pause menu

---
+++ BearTether/Assets/Scripts/Management/Pause.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject _pauseMenu;
+
+    public void SetPause()
+    {
+        _pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        _pauseMenu.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
