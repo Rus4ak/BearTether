86ea4e916a59e85aa278e40fab42e0334ec4cdbc
rus4ak <rus4ak331@gmail.com>
2025-06-23 01:25:44 +0300

    Add new levels in singleplayer

--- BearTether/Assets/Scripts/StoneTrigger.cs
+++ BearTether/Assets/Scripts/StoneTrigger.cs
@@ -6,6 +6,25 @@ public class StoneTrigger : MonoBehaviour
     [SerializeField] private float _force;
     [SerializeField] private int _direction;
 
+    private Vector3 _startPosition;
+    private Transform _player;
+
+    private void Start()
+    {
+        _startPosition = _stone.transform.position;
+        _player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (_player.position.x < 0)
+        {
+            _stone.linearVelocity = Vector3.zero;
+            _stone.angularVelocity = 0;
+            _stone.transform.position = _startPosition;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
